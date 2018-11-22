214c17bc17c038ec90eb37366b9412f4b3788b72
ChengzhangZhu <kevin.zhu.china@gmail.com>
2018-11-22 12:05:36 +1100

    initial

---
+++ COLDER/README.m
@@ -0,0 +1,18 @@
+# COLDER package
+
+## Introduction
+
+COLDER package is a tool for spam review detection with cold-start problem.
+This package is wrote by `python` with `Tensorflow`.
+
+## Major Files and Functions
+
++ `context_generator.py`: social and behavior context generation
++ `graph.py`: building graph from social network
++ `sample_generator.py`: training sample generation via negative sampling
++ `train.py`: training
++ `prediction.py`: prediction
+
+## Dependent Packages
++ tensorflow
++ numpy
