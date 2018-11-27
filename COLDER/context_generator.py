fixed overflow problem in exp in softmax

COLDER/context_generator.py
@@ -24,9 +24,9 @@ def weighted_random_walk_generator(g=Graph(), minT=1, maxT=32, p=0.15, max_lengt
     centrality = list()  # the centrality of each node, here use degree centrality
     total_degree = 0
     for node in nodes:
-        degree = g.degree[node]
+        degree.append(g.degree[node])
     degree = np.asarray(degree)
-    degree = (degree - np.min(degree))/(np.max(degree) - np.min(degree))  # normalize the degree to avoid overflow in exp
+    degree = (degree - np.min(degree))*1.0/(np.max(degree) - np.min(degree))  # normalize the degree to avoid overflow in exp
     for i in range(len(nodes)):
         total_degree += np.exp(degree[i])
         centrality.append(np.exp(degree[i]))