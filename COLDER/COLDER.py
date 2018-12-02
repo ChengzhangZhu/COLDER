a11ff1061578e2726d7395649258d4854e86bc96
ChengzhangZhu <kevin.zhu.china@gmail.com>
2018-12-02 19:07:28 +1100

    tune parameters

--- COLDER/COLDER.py
+++ COLDER/COLDER.py
@@ -107,7 +107,7 @@ class JointLoss(Layer):
         user_social_relation_loss = inputs[4]
         item_social_relation_loss = inputs[5]
         if alpha is None:
-            alpha = np.array([100,0.5,0.5,0.5])
+            alpha = np.array([1,0.5,0.5,0.5])
         loss = alpha[0]*(fraud_detection_loss_1+fraud_detection_loss_2) + alpha[1]*(behavior_success_loss_1+behavior_success_loss_2) + alpha[2]*user_social_relation_loss + alpha[3]*item_social_relation_loss
         self.add_loss(loss, inputs=inputs)
         return loss
@@ -140,7 +140,7 @@ class COLDER:
             fraud_detector_nodes = [100, 100]
         self.config['fraud_detector_nodes'] = fraud_detector_nodes  # the layer structure and nodes in the fraud detector
         if alpha is None:
-            alpha = [1, 0.5, 0.5, 0.5]
+            alpha = [10, 0.5, 0.5, 0.5]
         self.config['alpha'] = np.asarray(alpha)  # the coefficients of training losses
         self.config['loss_history'] = list()  # record the training loss in each epoch
     
